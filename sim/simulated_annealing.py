76c4e4e623b5f453db27e3d7739961676d63113f
AmandaCordero <amandacordero.1709@gmail.com>
2026-05-26 07:13:44 -0400

    last time

--- sim/simulated_annealing.py
+++ sim/simulated_annealing.py
@@ -5,6 +5,7 @@ from scipy.spatial import cKDTree
 from config import SIMULATION_PARAMS
 from main import sim
 import pandas as pd
+import time
 
 serotipos = {
     '1': ('pre PsC SP sp1', 'post PsC SP sp1'),
@@ -25,22 +26,29 @@ def vecino(parametros):
     nuevo[clave] = max(0, min(0.1, nuevo[clave] + delta))  # asegurar rango [0,1]
     return nuevo
 
-def recocido_simulado(param_inicial, temp_inicial=1.0, temp_final=1e-1, alpha=0.9, max_iter=100):
+def recocido_simulado(param_inicial,serotipo, temp_inicial=1.0, temp_final=1e-2, alpha=0.9, max_iter=100):
     estado_actual = param_inicial
     res = sim(estado_actual, 48)
-    energia_actual = metrica(res)
+    energia_actual = metrica(res, serotipo)
     T = temp_inicial
     mejor_estado = estado_actual
     mejor_energia = energia_actual
-
     i = 0
+    with open(f"log_sim.txt", "a") as f:
+            f.write(f"serotipo: {serotipo}\n")
+            f.write(f"corrida {i}: {param_inicial}\n")
+            f.write(f"energia {i}: {energia_actual}\n")
+
     while T > temp_final and i < max_iter:
         print(f"etapa {i} del recocido")
         print(f"Temperatura {T}")
         i+=1
         estado_nuevo = vecino(estado_actual)
         resn = sim(estado_nuevo,48)
-        energia_nueva = metrica(resn)
+        energia_nueva = metrica(resn, serotipo)
+        with open(f"log_sim.txt", "a") as f:
+            f.write(f"corrida {i}: {estado_nuevo}\n")
+            f.write(f"energia {i}: {energia_nueva}\n")
         delta = energia_nueva - energia_actual
         if delta < 0 or random.random() < math.exp(-delta / T):
             estado_actual = estado_nuevo
@@ -52,21 +60,20 @@ def recocido_simulado(param_inicial, temp_inicial=1.0, temp_final=1e-1, alpha=0.
         T *= alpha  # enfriamiento
     return mejor_estado, mejor_energia, res
 
-def metrica(antibodies):
+def metrica(antibodies, serotipo):
     df = pd.read_csv('sim/data/train_VCN7-Tf_fit.csv')
-    distancias_chamfer = 0
 
-    for serotipo, (col_pre, col_post) in serotipos.items():
-        # Extraer valores pre y post para ese serotipo
-        pre_vals = df[col_pre].values.reshape(-1, 1)
-        post_vals = df[col_post].values.reshape(-1, 1)
-        
-        points1 = [(pre_vals[i], post_vals[i]) for i in range(len(pre_vals))]
-        points2 = [(antibodies[i][serotipo][0], antibodies[i][serotipo][0]) for i in range(len(antibodies))]
+    # for serotipo, (col_pre, col_post) in serotipos.items():
+    # Extraer valores pre y post para ese serotipo
+    pre_vals = df[serotipos[serotipo][0]].values.reshape(-1, 1)
+    post_vals = df[serotipos[serotipo][1]].values.reshape(-1, 1)
+    
+    points1 = [(pre_vals[i], post_vals[i]) for i in range(len(pre_vals))]
+    points2 = [(antibodies[i][serotipo][0], antibodies[i][serotipo][1]) for i in range(len(antibodies))]
 
-        distancias_chamfer += chamfer_distance(points1, points2)
+    distancias_chamfer = chamfer_distance(points1, points2)
 
-    return distancias_chamfer/7
+    return distancias_chamfer
 
 
 
@@ -92,19 +99,29 @@ def chamfer_distance(points1, points2):
 
     # Promedio de ambas direcciones
     chamfer_dist = np.mean(dist1) + np.mean(dist2)
-    return 1/(1 +chamfer_dist)
+    return chamfer_dist
 
 
 if __name__ == "__main__":
-    a = recocido_simulado(SIMULATION_PARAMS)
-    # print(a)
-    lista = a[2]
-    dictio = {}
+    inicio = time.perf_counter()
     for s in serotipos.keys():
-        dictio[s] = []
-    for elem in lista:
-        for s in serotipos.keys():
-            dictio[s].append(elem[s])
+        print(f"Serotipo {s}")
+        a = recocido_simulado(SIMULATION_PARAMS, s)
+        with open(f"log_sim.txt", "a") as f:
+            f.write(f"serotipo: {s}\n")
+            f.write(f"best params: {a[0]}\n")
+            f.write(f"best energy: {a[1]}\n")
 
-    with open("log_simulacion.txt", "w") as f:
-        f.write(f"{dictio}")
+        lista = a[2]
+        dictio = {}
+        for ser in serotipos.keys():
+            dictio[ser] = []
+        for elem in lista:
+            for se in serotipos.keys():
+                dictio[se].append(elem[se])
+
+        with open(f"log_simulacion{s}.txt", "w") as f:
+            f.write(f"{dictio}")
+
+    fin = time.perf_counter()
+    print(fin-inicio)
